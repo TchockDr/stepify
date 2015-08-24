65e4f8264b51a7bde00460361e2f2474f8c8ec39
TchockDr <rgoscar@gmail.com>
2015-08-24 15:37:24 -0300

    Update stepify.js

--- stepify.js
+++ stepify.js
@@ -1,114 +1,177 @@
-(function($, doc, win){
+/*!
+ * jQuery progress tracker generation plugin
+ * Author: Oscar Romero  
+ */
+;(function($, doc, win){
 	
-	var self           = null;	
-	var currentStep    = null;	
-	var defaults = {
+	this.self        = null;	
+	this.defaults    = {
 			startAt: null,
-			contentMapping: null
+			stepsDef: null
 	};
 	
-	Steps.prototype.forward = function(){
-		currentStep.callNext();
+	Steps.prototype.forward = function(content){
+		Step.prototype.getCurrentStep().callNext(content);
 	};
 	
 	Steps.prototype.back = function(){
-		currentStep.callPrevious();
+		Step.prototype.getCurrentStep().callPrevious();
 	};
 	
-	function Step(stepOptions, HTMLNode){
+	// Represents a Step
+	function Step(stepOptions){
 		
-		var step = this;
+		var thisStep = this;
 		
+		// Custom styles for the different states
 		var TODO_STYLE    = 'steptracker-todo';
 		var DONE_STYLE    = 'steptracker-done';
 		var CURRENT_STYLE = 'steptracker-current';
 		
+		// Options to be used as default
 		var defaults = {
 				visible: false,
 				loaded: false,
-				HTMLNode: null,
+				HTMLElement: null,
 				contaniner: null,
 				content: null,
 				id: null,
+				active: false,
 				onRender: function(){
-					step.setVisible();
+					thisStep.setVisible();
 				}
 		};
 		
-		this.options = $.extend(defaults, stepOptions);	
+		// All created steps act like a double linked list for easy traversing.		
+		if(this.last){
+			this.last.next = this;
+			this.prev = this.last;
+		}
+		 
+		Step.prototype.last = this;
 		
-		this.options.HTMLNode = HTMLNode;
-		$(step).on('rendered', step.options.onRender);
+		// Step options are generated with both default and custom definitions
+		this.options = $.extend(defaults, stepOptions);
 		
-		HTMLNode.attr('id', this.options.id);
-		HTMLNode.attr('class', TODO_STYLE);
+		// The <li> HTML element is created for this Step
+		this.options.HTMLElement = $('<li></li>').attr('id', this.options.id).attr('class', TODO_STYLE).append(this.options.title);
+		// The <div> HTML element that will hold the content for this step is also created
+		this.options.container =  $('<div></div>').attr('id', this.options.id + 'Content');
 		
-		/**
-		 * Sets the step visible in the "time line", hidding everything else.
-		 */
+		// This is pretty self explanatory
+		$(thisStep).on('rendered', thisStep.options.onRender);
+		
+		$(thisStep.options.HTMLElement).click(function(){
+			
+			if(thisStep.isLoaded() || thisStep.prev.isLoaded()){
+				if(!thisStep.isLoaded()){
+					thisStep.render();
+				}
+				thisStep.setVisible();
+			}
+			
+		});		
+		
+		var buffer = [];
+		
+		// If the step is marked to be the current one activated, the all steps before are marked as done
+		if(this.options.active){
+			
+			var previousStep = this.prev;			
+			
+			while(previousStep){
+				
+				buffer.push(previousStep.render());
+				
+				previousStep.options.visible = false;
+				
+				$(previousStep.getContainer()).hide();
+				
+				previousStep.setState('DONE');
+				previousStep = previousStep.prev;
+				
+			}
+			
+			// The current step is rendered once all the previous ones are ready	
+			$.when.apply($, buffer).done(function(){
+				thisStep.render();
+			});
+			
+		}
+		
+		// Sets the step as the current one selected
 		this.setVisible = function(){
 
-			// Previous steps are hidden
-			var current = step.prev;
+			// All steps before this one are hidden
+			var current = thisStep.prev;
 			while(current){				
 				current.hide();
 				current = current.prev;
 			}
 
-			// Steps ahead in the time line are also hidden
-			current = step.next;
+			// All steps after this one are also hidden
+			current = thisStep.next;
 			while(current){				
 				current.hide();
 				current = current.next;
 			}
 			
-			// Step is shown
-			step.getContainer().show();
-			step.setState('DONE');		
-			step.options.visible = true;
+			// The Step is shown
+			thisStep.getContainer().show();
+			thisStep.setState('DONE');		
+			thisStep.options.visible = true;
+			thisStep.setState('CURRENT');
 			
-			step.setState('CURRENT');
-			
-			currentStep = step;
+			// Se actualiza el paso actual con el que se está dejando activo
+			Step.prototype.currentStep = thisStep;
 			
 		};
 		
 		this.hide = function(){
-			step.options.visible = false;
-			step.getContainer().hide();
-			$(step.options.HTMLNode).removeClass(CURRENT_STYLE);
+			thisStep.options.visible = false;
+			thisStep.getContainer().hide();
+			$(thisStep.options.HTMLElement).removeClass(CURRENT_STYLE);
 		};
 		
 		/**
 		 * Despliega por pantalla el contenido definido para la etapa
 		 */
-		this.render = function(){
+		this.render = function(content){
+
 			// Solo se va a buscar el contenido de la etapa si es que este aun no ha sido cargado
-			if(step.isLoaded()){ 
-				$(step).trigger('rendered');
-				return; 
+			if(thisStep.isLoaded()){ 
+				$(thisStep).trigger('rendered');
+			}else if(thisStep.options.content){
+				return $.ajax({
+					method: "GET",
+					url: thisStep.options.content,
+					success: function(data){
+						doRender(data);
+					}
+				});
+			}else if(content){
+				doRender(content);
 			}
-			return $.ajax({
-				method: "GET",
-				url: step.options.content,
-				success: function(data){
-					var stepContainer = $('<div></div>').attr('id', step.options.id + 'Content').append(data);
-					step.options.container = $(stepContainer.get(0));
-					$('#stepsContentContainer').append(stepContainer);
-					step.setLoaded(true);
-					$(step).trigger('rendered');
-				}
-			});
+			
+			function doRender(data){
+				thisStep.options.container.append(data);
+				thisStep.options.contentTarget.append(thisStep.options.container);
+				thisStep.options.loaded = true;
+				$(thisStep).trigger('rendered');
+			}
+			
+			return;
+			
 		};
 		
 		this.setState = function(state){
 			switch (state) {
 		    case "DONE":
-		    	$(step.options.HTMLNode).removeClass(TODO_STYLE);
-				$(step.options.HTMLNode).addClass(DONE_STYLE);
+		    	$(thisStep.options.HTMLElement).removeClass(TODO_STYLE);
+				$(thisStep.options.HTMLElement).addClass(DONE_STYLE);
 		        break;
 		    case "CURRENT":
-		    	$(step.options.HTMLNode).addClass(CURRENT_STYLE);
+		    	$(thisStep.options.HTMLElement).addClass(CURRENT_STYLE);
 		    	break;
 			};
 		};
@@ -117,21 +180,13 @@
 			return $(this.options.container);
 		};
 		
-		this.isVisible = function(){
-			return step.options.visible;
-		};
-		
 		this.isLoaded = function(){
-			return step.options.loaded;
+			return thisStep.options.loaded;
 		};
 		
-		this.setLoaded = function(loaded){
-			step.options.loaded = loaded;
-		};
-		
-		this.callNext = function(){
+		this.callNext = function(content){
 			if(this.next){
-				this.next.render();
+				this.next.render(content);
 			}
 		};
 		
@@ -141,6 +196,10 @@
 			}			
 		};
 		
+		Step.prototype.getCurrentStep = function(){
+			return Step.prototype.currentStep;
+		};
+		
 	}
 	
 	function Steps(containerElement, options){
@@ -148,82 +207,19 @@
 		self.containerElement = $(containerElement);
 		self.options = $.extend(defaults, options);
 		init();
-	}
-	
-	/**
-	 * Precarga los contenidos de las etapas hasta una etapa en particular
-	 * @finalStep Etapa hasta donde se necesita precargar los contenidos
-	 */
-	function preloadSteps(finalStep){
-		
-		var lastStep = null;		
-		var buffer = [];		
-		
-		// Se cargan los contenidos de todas las etapas hasta la etapa indicada
-		$.each(self.options.contentMapping, function(index, step){
-			
-			// Se almacenan las llamadas ajax a los contenidos
-			buffer.push(step.render());				
-			
-			// Por defecto no se marcan como visibles
-			step.options.visible = false;
-			$(step.getContainer()).hide();	
-			
-			// Se marcan como realizados
-			step.setState('DONE');
-			
-			// Se almacena la última etapa
-			if(finalStep == step.options.id){
-				lastStep = step; 
-				currentStep = lastStep;
-				return false;
-			}
-			
-		});
-		
-		// Cuando se terminan de cargar los contendios se muestra la útlima
-		$.when.apply($, buffer).done(function(){			
-			lastStep.setVisible(); //updateVisibility(lastStep);
-		});
-		
-	}
+	}	
 	
 	function init(){
 		
-		var previousStep = null;
-		
-		$.each(self.options.contentMapping, function(index, element){
-						
-			self.options.contentMapping[index] = new Step(element, $(self.containerElement.children()[index]));
-			element = self.options.contentMapping[index];
-
-			if(previousStep){
-				previousStep.next = element;
-				element.prev = previousStep;
+		$.each(self.options.stepsDef, function(index, stepDef){
+			
+			if(self.options.showContentAt){
+				stepDef.contentTarget = self.options.showContentAt;
 			}
 			
-			previousStep = element;
+			self.containerElement.append(new Step(stepDef).options.HTMLElement);
 			
-			$(element.options.HTMLNode).click(function(){
-				
-				// Se activan solo las etapas que ya se hayan cargado o las que esten a continuación
-				// de una ya cargada
-				if(element.isLoaded() || element.prev.isLoaded()){
-					currentStep = element;
-					if(!currentStep.isLoaded()){
-						currentStep.render();
-					}
-					currentStep.setVisible();
-				}
-				
-			});			
-			
-						
 		});
-		
-		if(self.options.startAt){
-			preloadSteps(self.options.startAt);
-		}
 		
 	}
 	
